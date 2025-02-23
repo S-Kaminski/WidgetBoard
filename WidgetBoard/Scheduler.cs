c8fa773f28988bd2440edb967628690e9717229a
Szymon Kamiński <szy.kaminski@gmail.com>
2025-02-23 01:17:53 +0100

    Add new class to allow widgets to schedule an action

---
+++ WidgetBoard/Scheduler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WidgetBoard
+{
+    public class Scheduler
+    {
+        public void ScheduleAction(TimeSpan timeSpan, Action action)
+        {
+            Task.Run(async () =>
+            {
+                await Task.Delay(timeSpan);
+                action.Invoke();
+            });
+        }
+    }
+}
