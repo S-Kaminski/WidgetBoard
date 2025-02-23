cb5c1a6160d6b11204bd0ece56505adc50172322
Szymon Kamiński <szy.kaminski@gmail.com>
2025-02-23 01:06:06 +0100

    Add example of base class for view models

---
+++ WidgetBoard/ViewModels/BaseViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WidgetBoard.ViewModels
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected bool SetProperty<TValue>(ref TValue backingField, TValue value, [CallerMemberName] string propertyName = "")
+        {
+            if (Comparer<TValue>.Default.Compare(backingField,value) == 0)
+            {
+                return false;
+            }
+            backingField = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+
+}
+
