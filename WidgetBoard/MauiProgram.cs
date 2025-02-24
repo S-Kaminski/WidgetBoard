6b829e607cd225dbdbe7af3ed007dc11e42d1616
Szymon Kamiński <szy.kaminski@gmail.com>
2025-02-24 03:10:08 +0100

    Register service for created AppShell view model

--- WidgetBoard/MauiProgram.cs
+++ WidgetBoard/MauiProgram.cs
@@ -1,27 +1,29 @@
 ﻿using Microsoft.Extensions.Logging;
 using WidgetBoard.Pages;
+using WidgetBoard.ViewModels;
 
 namespace WidgetBoard;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
-		builder.Services.AddTransient<BoardDetailsPage>();
-		builder.Services.AddTransient<FixedBoardPage>();
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+        builder.Services.AddTransient<BoardDetailsPage>();
+        builder.Services.AddTransient<FixedBoardPage>();
+        builder.Services.AddTransient<AppShellViewModel>();
 
 #if DEBUG
         builder.Logging.AddDebug();
 #endif
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
