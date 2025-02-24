1924727e1a034b3eed9d69233751124309bb7246
Szymon Kamiński <szy.kaminski@gmail.com>
2025-02-24 02:46:13 +0100

    Add ClockWidgetViewModel and logic

---
+++ WidgetBoard/ViewModels/ClockWidgetViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace WidgetBoard.ViewModels
+{
+    public class ClockWidgetViewModel : BaseViewModel, IWidgetViewModel //must be partial
+    {
+        private DateTime time;
+
+        public DateTime Time
+        {
+            get => time;
+            set => SetProperty(ref time, value);
+        }
+
+        private readonly Scheduler scheduler = new();
+        public int Position { get; set; }
+        public string Type => "Clock";
+
+        public ClockWidgetViewModel()
+        {
+            SetTime(DateTime.Now);
+        }
+
+        public void SetTime(DateTime dateTime)
+        {
+            Time = dateTime;
+
+            scheduler.ScheduleAction(TimeSpan.FromSeconds(1), () => SetTime(DateTime.Now));
+        }
+    }
+}
