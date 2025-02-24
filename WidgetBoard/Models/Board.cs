8f4466590ce28635bc891a975a767e7330531027
Szymon Kamiński <szy.kaminski@gmail.com>
2025-02-24 02:57:28 +0100

    Add Board.cs to represent overall board

---
+++ WidgetBoard/Models/Board.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WidgetBoard.Models
+{
+    public class Board
+    {
+        public required string Name { get; init; }
+        public required BaseLayout Layout { get; init; }
+    }
+}
