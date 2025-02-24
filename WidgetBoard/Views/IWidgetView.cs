ba3e7d49dd38c586994961bdd2921a5f37c4de37
Szymon Kamiński <szy.kaminski@gmail.com>
2025-02-24 02:47:17 +0100

    Add IWidgetView and ClockWidgetView

---
+++ WidgetBoard/Views/IWidgetView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WidgetBoard.ViewModels;
+
+namespace WidgetBoard.Views
+{
+    public interface IWidgetView
+    {
+        int Position
+        {
+            get => WidgetViewModel.Position;
+            set => WidgetViewModel.Position = value;
+        }
+
+        IWidgetViewModel WidgetViewModel { get; set; }
+    }
+}
