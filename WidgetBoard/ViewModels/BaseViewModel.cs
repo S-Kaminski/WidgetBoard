d7b5a54af90d5546de53695dbf01c30f3c6b60a7
Szymon Kamiński <szy.kaminski@gmail.com>
2025-02-24 02:38:48 +0100

    Add access modifier and clean formating

--- WidgetBoard/ViewModels/BaseViewModel.cs
+++ WidgetBoard/ViewModels/BaseViewModel.cs
@@ -8,16 +8,18 @@ using System.Runtime.CompilerServices;
 
 namespace WidgetBoard.ViewModels
 {
-    class BaseViewModel : INotifyPropertyChanged
+    public class BaseViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
         protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
         protected bool SetProperty<TValue>(ref TValue backingField, TValue value, [CallerMemberName] string propertyName = "")
         {
-            if (Comparer<TValue>.Default.Compare(backingField,value) == 0)
+            if (Comparer<TValue>.Default.Compare(backingField, value) == 0)
             {
                 return false;
             }
@@ -26,6 +28,4 @@ namespace WidgetBoard.ViewModels
             return true;
         }
     }
-
 }
-
