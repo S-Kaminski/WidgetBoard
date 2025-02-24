1a0f0b032bb795f3326abaf790935a9c83ceddab
Szymon Kamiński <szy.kaminski@gmail.com>
2025-02-24 02:55:02 +0100

    Add FixedLayout class inheriting from BaseLayout class and properties to it

---
+++ WidgetBoard/Models/FixedLayout.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WidgetBoard.Models
+{
+    public class FixedLayout : BaseLayout
+    {
+        public int NumberOfColumns { get; set; }
+        public int NumberOfRows { get; set; }
+    }
+}
