c300b390d03a17e8b25c6072563e3228a68fb8a7
Szymon Kamiński <szy.kaminski@gmail.com>
2025-03-01 18:54:53 +0100

    Add property BoardName and inherit BaseViewModel to get notifications

--- WidgetBoard/ViewModels/BoardDetailsPageViewModel.cs
+++ WidgetBoard/ViewModels/BoardDetailsPageViewModel.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static WidgetBoard.ViewModels.BaseViewModel;
 
 namespace WidgetBoard.ViewModels
 {
-    class BoardDetailsPageViewModel
+    public class BoardDetailsPageViewModel : BaseViewModel
     {
+        private string boardName;
+
+        public string BoardName
+        {
+            get => boardName;
+            set => SetProperty(ref boardName, value);
+        }
     }
 }
