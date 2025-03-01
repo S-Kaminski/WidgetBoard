ac4a86be5793cc43cbfea4d7066ae413cd436eac
Szymon Kamiński <szy.kaminski@gmail.com>
2025-03-01 19:48:59 +0100

    Change format from DateTime to two properties (TimeOnly/DateOnly)

--- WidgetBoard/ViewModels/ClockWidgetViewModel.cs
+++ WidgetBoard/ViewModels/ClockWidgetViewModel.cs
@@ -10,26 +10,37 @@ namespace WidgetBoard.ViewModels
 {
     public class ClockWidgetViewModel : BaseViewModel, IWidgetViewModel //must be partial
     {
-        private DateTime time;
-
-        public DateTime Time
-        {
-            get => time;
-            set => SetProperty(ref time, value);
-        }
-
         private readonly Scheduler scheduler = new();
-        public int Position { get; set; }
-        public string Type => "Clock";
+
+        private DateOnly date;
+
+        private TimeOnly time;
 
         public ClockWidgetViewModel()
         {
             SetTime(DateTime.Now);
         }
 
+        public DateOnly Date
+        {
+            get => date;
+            set => SetProperty(ref date, value);
+        }
+
+        public int Position { get; set; }
+
+        public TimeOnly Time
+        {
+            get => time;
+            set => SetProperty(ref time, value);
+        }
+
+        public string Type => "Clock";
+
         public void SetTime(DateTime dateTime)
         {
-            Time = dateTime;
+            Date = DateOnly.FromDateTime(dateTime);
+            Time = TimeOnly.FromDateTime(dateTime);
 
             scheduler.ScheduleAction(TimeSpan.FromSeconds(1), () => SetTime(DateTime.Now));
         }
