eeef5bc3fdb5f1edae407c5f34ede829ade5d06e
Szymon Kamiński <szy.kaminski@gmail.com>
2025-03-01 19:46:47 +0100

    Add constructor and implementation of SaveCommand, change setter of BoardName to send ChanceCanExecute to SaveCommand

--- WidgetBoard/ViewModels/BoardDetailsPageViewModel.cs
+++ WidgetBoard/ViewModels/BoardDetailsPageViewModel.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WidgetBoard.Models;
 using static WidgetBoard.ViewModels.BaseViewModel;
 
 namespace WidgetBoard.ViewModels
 {
     public class BoardDetailsPageViewModel : BaseViewModel
     {
+        public BoardDetailsPageViewModel()
+        {
+            SaveCommand = new Command(
+                () => Save(),
+                () => !string.IsNullOrWhiteSpace(BoardName));
+        }
+
         private string boardName;
         private bool isFixed = true;
         private int numberOfColumns = 3;
@@ -17,7 +25,11 @@ namespace WidgetBoard.ViewModels
         public string BoardName
         {
             get => boardName;
-            set => SetProperty(ref boardName, value);
+            set
+            {
+                SetProperty(ref boardName, value);
+                SaveCommand.ChangeCanExecute();
+            }
         }
 
         public bool IsFixed
@@ -36,6 +48,21 @@ namespace WidgetBoard.ViewModels
         {
             get => numberOfRows;
             set => SetProperty(ref numberOfRows, value);
+        }
+
+        public Command SaveCommand { get; }
+
+        private void Save()
+        {
+            Board board = new()
+            {
+                Name = BoardName,
+                Layout = new FixedLayout
+                {
+                    NumberOfColumns = NumberOfColumns,
+                    NumberOfRows = NumberOfRows
+                }
+            };
         }
     }
 }
