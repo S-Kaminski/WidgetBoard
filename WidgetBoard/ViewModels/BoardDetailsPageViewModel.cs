f40db159e1d0060a1eaab5c101aa2933dd9f5a54
Szymon Kamiński <szy.kaminski@gmail.com>
2025-03-01 19:04:15 +0100

    Add fields and properties to bind to

--- WidgetBoard/ViewModels/BoardDetailsPageViewModel.cs
+++ WidgetBoard/ViewModels/BoardDetailsPageViewModel.cs
@@ -10,11 +10,32 @@ namespace WidgetBoard.ViewModels
     public class BoardDetailsPageViewModel : BaseViewModel
     {
         private string boardName;
+        private bool isFixed = true;
+        private int numberOfColumns = 3;
+        private int numberOfRows = 2;
 
         public string BoardName
         {
             get => boardName;
             set => SetProperty(ref boardName, value);
+        }
+
+        public bool IsFixed
+        {
+            get => isFixed;
+            set => SetProperty(ref isFixed, value);
+        }
+
+        public int NumberOfColumns
+        {
+            get => numberOfColumns;
+            set => SetProperty(ref numberOfColumns, value);
+        }
+
+        public int NumberOfRows
+        {
+            get => numberOfRows;
+            set => SetProperty(ref numberOfRows, value);
         }
     }
 }
