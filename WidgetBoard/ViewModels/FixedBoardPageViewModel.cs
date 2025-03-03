4b2c652cdb03b26d21f96c6cf7cd0fc578290a9a
Szymon Kamiński <szy.kaminski@gmail.com>
2025-03-03 01:22:27 +0100

    Create view model and implement IQueryAttributable interface for future usage

---
+++ WidgetBoard/ViewModels/FixedBoardPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WidgetBoard.Models;
+
+namespace WidgetBoard.ViewModels
+{
+    public class FixedBoardPageViewModel : BaseViewModel, IQueryAttributable
+    {
+        public void ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+            var board = query["Board"] as Board;
+        }
+    }
+}
